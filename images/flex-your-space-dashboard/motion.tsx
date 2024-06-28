9876add4795ba194cdccc5ee1f5ae44c5dba4205
Angelika Maglinte <53634332+angelikamaglinte@users.noreply.github.com>
2024-06-27 22:32:37 -0600

    Updated Flex Your Space Dashboard

---
+++ images/flex-your-space-dashboard/motion.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { motion, TargetAndTransition } from 'framer-motion';
+
+const animations: Record<string, TargetAndTransition> = {}
+
+export function App() {
+  return (
+    <>
+      
+    </>
+  )
+}
